b653dc917e4241167ab92ff311a69d53f62b121d
haticebabaoglu <haticebaba63@gmail.com>
2024-12-01 23:54:12 +0300

    update

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,18 +1,21 @@
-import { cn } from "@/lib/utils"
-import { ReactNode } from "react"
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
 const MaxWidthWrapper = ({
-    classname,
-    children
-}:{
-    classname?: string
-    children: ReactNode
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
 }) => {
   return (
-    <div className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20", classname)}>
-         {children}
+    <div
+      className={cn(
+        'h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
+        className
+      )}>
+      {children}
     </div>
-   
   )
 }
 
