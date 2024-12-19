7422ea9c3f47cc9bede94c00e77b1bd0d6321975
haticebabaoglu <haticebaba63@gmail.com>
2024-12-19 18:49:58 +0300

    .

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -9,7 +9,6 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
  
 const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return ( 
-    
     <div
       className={cn(
         'relative pointer-events-none z-50 overflow-hidden',
